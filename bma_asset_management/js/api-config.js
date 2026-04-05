821969485ff017169938aeb500c808583edad244
weerachaiartdesign <weerachaiartdesign@gmail.com>
2026-04-05 14:19:06 +0700

    Update api-config.js

--- bma_asset_management/js/api-config.js
+++ bma_asset_management/js/api-config.js
@@ -4,4 +4,4 @@
  */
 
 // นำ Web App URL ที่ได้จากการ Deploy Google Apps Script มาวางที่นี่
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbyMrBNreh9AqM1GtyVZlQpbfIUZxIrKIh0gJc_JkIjBqyNvFmMHiVo5U9aESKqpB5HJ/exec";
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxMHkbtd4HryC0h3eArQUP_w8hrl_jLRwDMho7aU4hRiFnqGqHz7t9GNsZ8jq-iqfo/exec";
